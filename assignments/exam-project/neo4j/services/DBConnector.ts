e18f388cbd2ab82433b887b61aaeeb8fc553ae95
Mohammad Omar Salah Hariri <mosh@proactive.dk>
2020-05-21 14:32:35 +0200

    add check connection

--- assignments/exam-project/neo4j/services/DBConnector.ts
+++ assignments/exam-project/neo4j/services/DBConnector.ts
@@ -1,6 +1,18 @@
 import neo4j from 'neo4j-driver';
+import colors from 'colors';
 
 var driver = neo4j.driver('bolt://localhost', neo4j.auth.basic('neo4j', 'password'));
 const session = driver.session();
+
+(() => {
+  session.readTransaction(async tx => {
+    const result = await tx.run('MATCH (n) RETURN n');
+    if (result) {
+      console.log(colors.green('Neo4j is connected'));
+    } else {
+      console.log(colors.red('Neo4j is not connected'));
+    }
+  });
+})();
 
 export { session as connection };
