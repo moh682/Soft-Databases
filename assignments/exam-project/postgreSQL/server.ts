e0eeba54d1add7c286a4897fb086e6aa0ed4e7cb
Mohammad Omar Salah Hariri <mosh@proactive.dk>
2020-05-26 16:33:26 +0200

    fix double connection print

--- assignments/exam-project/postgreSQL/server.ts
+++ assignments/exam-project/postgreSQL/server.ts
@@ -3,15 +3,11 @@ import bodyParser from 'body-parser';
 import { AuthenticationMiddleware } from './middleware/Authentication.middleware';
 import { LoggerRoute } from './routes/Logger.route';
 import { LoggingMapper } from './mappers/Logger.mapper';
-import connector from './services/DBConnector';
 import { SQL_PORT } from '../constants';
 
 const loggerMapper = new LoggingMapper();
 try {
-  connector
-    .getPool()
-    .connect()
-    .then(() => loggerMapper.injectFile());
+  loggerMapper.injectFile();
 } catch (ex) {
   console.log(ex);
 }
--- assignments/exam-project/postgreSQL/services/DBConnector.ts
+++ assignments/exam-project/postgreSQL/services/DBConnector.ts
@@ -1,9 +1,8 @@
-import { Pool, Client } from 'pg';
+import { Pool } from 'pg';
 import colors from 'colors';
 import { SQL_DATABASE, SQL_PASSWORD, SQL_USERNAME } from '../../constants';
 
 let pool: Pool;
-let client: Client;
 export default {
   getPool: function () {
     if (pool) return pool;
@@ -16,14 +15,5 @@ export default {
       console.log(colors.green('PostgreSQL is connected'));
     });
     return pool;
-  },
-  getClient: function () {
-    if (client) return client;
-    client = new Client({
-      database: SQL_DATABASE,
-      user: SQL_USERNAME,
-      password: SQL_PASSWORD,
-    });
-    return client;
   },
 };
