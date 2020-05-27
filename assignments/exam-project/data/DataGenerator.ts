edb187a9056755e4db8c68758f0288cc84ac6ec3
Mohammad Omar Salah Hariri <mosh@proactive.dk>
2020-05-27 13:38:09 +0200

    fix db

--- assignments/exam-project/data/DataGenerator.ts
+++ assignments/exam-project/data/DataGenerator.ts
@@ -1,5 +1,22 @@
-import {} from 'fs';
+import { readFileSync, createReadStream } from 'fs';
 
 const getUsers = async () => {
-	new Promise<>
-}
+  new Promise(resolve => {
+    const users_text = readFileSync(__dirname + '/users.json', 'utf8');
+    resolve(JSON.parse(users_text));
+  });
+};
+
+const getText = async () => {
+  new Promise(resolve => {
+    const data = [];
+    const users_text = createReadStream(__dirname + '/text.text', 'utf8')
+      .on('data', str => data.push(str))
+      .on('end', () => {
+        console.log(data);
+      });
+  });
+};
+
+getUsers();
+getText();
--- assignments/exam-project/postgreSQL/services/DBConnector.ts
+++ assignments/exam-project/postgreSQL/services/DBConnector.ts
@@ -1,6 +1,6 @@
 import { Pool } from 'pg';
 import colors from 'colors';
-import { SQL_PASSWORD, SQL_USERNAME } from '../../constants';
+import { SQL_PASSWORD, SQL_USERNAME, SQL_DATABASE } from '../../constants';
 
 let pool: Pool;
 export default {
@@ -9,6 +9,7 @@ export default {
     pool = new Pool({
       user: SQL_USERNAME,
       password: SQL_PASSWORD,
+      database: SQL_DATABASE,
     });
     pool.on('connect', () => {
       console.log(colors.green('PostgreSQL is connected'));
